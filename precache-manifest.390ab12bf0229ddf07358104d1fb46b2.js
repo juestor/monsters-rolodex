80a51d69232ce5dba189fa7b3584e2b5da53c39b
juestor <jtorres@psl.com.co>
2020-05-03 16:24:24 -0500

    Updates

---
+++ precache-manifest.390ab12bf0229ddf07358104d1fb46b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4eb1ec2ab5095c6920469aeeca96162",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e7784ec38a981c213908",
+    "url": "/monsters-rolodex/static/css/main.38b34126.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7784ec38a981c213908",
+    "url": "/monsters-rolodex/static/js/main.71a34a7d.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
